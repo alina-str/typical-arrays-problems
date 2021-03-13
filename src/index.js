95645107f5f349eace4549393b853812db61f60d
alina-str <al-strukova@mail.ru>
2021-03-13 05:14:20 +0300

    feat: fix

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ exports.min = function min (array) {
   }
   if(array.length==0) min=0;
   if (array===undefined) min=0;
-  if (array.isArray(emptyArray) && emptyArray.length) min=0;
-  if (array.isArray(nonExistantArray) && nonExistantArray.length) min=0;
+  if (array.isArray(emptyArray)) min=0;
+  if (array.isArray(nonExistantArray)) min=0;
   return min;
 }
 
@@ -20,8 +20,8 @@ exports.max = function max (array) {
   }
   if(array.length==0) max=0;
   if (array===undefined) max=0;
-  if (array.isArray(emptyArray) && emptyArray.length) max=0;
-  if (array.isArray(nonExistantArray) && nonExistantArray.length) max=0;
+  if (array.isArray(emptyArray)) max=0;
+  if (array.isArray(nonExistantArray)) max=0;
   return max;
 }
 
@@ -34,7 +34,7 @@ exports.avg = function avg (array) {
   avg=s/array.length;
   if(array.length==0) avg=0;
   if (array===undefined) avg=0;
-  if (array.isArray(emptyArray) && emptyArray.length) avg=0;
-  if (array.isArray(nonExistantArray) && nonExistantArray.length) avg=0;
+  if (array.isArray(emptyArray)) avg=0;
+  if (array.isArray(nonExistantArray)) avg=0;
   return avg;
 }
