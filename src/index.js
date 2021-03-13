fc1d6adfa93c762d092e98f37d0105a8d69b578b
alina-str <al-strukova@mail.ru>
2021-03-13 05:34:10 +0300

    feat: fix

--- src/index.js
+++ src/index.js
@@ -1,35 +1,29 @@
 
 exports.min = function min (array) {
+  if (!(Array.isArray(array)) && !(array.length)) return 0;
   var min=array[0];
   for (let i=0; i<array.length; i++){
     if (array[i]<min) min=array[i];
   }
-  if(array.length==0) min=0;
-  if (array===undefined) min=0;
-  if (!(Array.isArray(array)) && !(array.length)) min=0;
   return min;
 }
 
 exports.max = function max (array) {
+  if (!(Array.isArray(array)) && !(array.length)) return 0;
   var max=array[0];
   for (let i=0; i<array.length; i++){
     if (array[i]>max) max=array[i];
   }
-  if(array.length==0) max=0;
-  if (array===undefined) max=0;
-  if (!(Array.isArray(array))&& !(array.length)) max=0;
   return max;
 }
 
 exports.avg = function avg (array) {
+  if (!(Array.isArray(array)) && !(array.length)) return 0;
   var avg=0;
   var s=0;
   for (let i=0; i<array.length; i++){
     s+=array[i];
   }
   avg=s/array.length;
-  if(array.length==0) avg=0;
-  if (array===undefined) avg=0;
-  if (!(Array.isArray(array))&& !(array.length)) avg=0;
   return avg;
 }
