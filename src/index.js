c337e041c9e3827875440b09cd16c33b9cca27c7
alina-str <al-strukova@mail.ru>
2021-03-13 05:27:35 +0300

    feat: fix

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ exports.min = function min (array) {
   }
   if(array.length==0) min=0;
   if (array===undefined) min=0;
-  if (!(Array.isArray(array))) min=0;
+  if (!(Array.isArray(array) && array.length)) min=0;
   return min;
 }
 
@@ -17,7 +17,7 @@ exports.max = function max (array) {
   }
   if(array.length==0) max=0;
   if (array===undefined) max=0;
-  if (!(Array.isArray(array))) max=0;
+  if (!(Array.isArray(array)&& array.length)) max=0;
   return max;
 }
 
@@ -30,6 +30,6 @@ exports.avg = function avg (array) {
   avg=s/array.length;
   if(array.length==0) avg=0;
   if (array===undefined) avg=0;
-  if (!(Array.isArray(array))) avg=0;
+  if (!(Array.isArray(array)&& array.length)) avg=0;
   return avg;
 }
