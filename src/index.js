9afbd89bedf701aad4e6bf9d9b8d734a2d12365b
alina-str <al-strukova@mail.ru>
2021-03-13 03:49:10 +0300

    feat: fix

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+  min=array[0];
+  for (let i=1; i<array.length; i++){
+    if (array[i]<min) min=array[i];
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  max=array[0];
+  for (let i=1; i<array.length; i++){
+    if (array[i]>max) max=array[i];
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let avg=0;
+  var k=0;
+  var s=0;
+  for (let i=0; i<array.length; i++){
+    s+=array[i]; k++;
+  }
+  avg=s/k;
+  return avg;
 }
